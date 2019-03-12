2058422da504388b47ea6e2a102a46a46a98d948
sashkomatviychuk <sashko1921@gmail.com>
2019-03-12 19:54:35 +0100

    Removed unused files

--- src/store/streams/cards.js
+++
@@ -1,7 +0,0 @@
-import { state$, store, getIn } from 'store';
-
-const getWeatherList = getIn(['cards', 'weatherList'], []);
-
-export const weatherList$ = state$
-    .map(state => getWeatherList(state))
-    .toProperty(() => getWeatherList(store.getState()));
--- src/store/streams/modal.js
+++
@@ -1,8 +0,0 @@
-import { state$, store, getIn } from 'store';
-
-const getIsOpen = getIn(['modal', 'isOpen'], false);
-
-export const isOpen$ = state$
-    .map(getIsOpen)
-    .toProperty(() => getIsOpen(store.getState()))
-    .skipDuplicates();
